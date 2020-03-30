42804d34a20b3c45f06b5b5232e8908fce5fbc88
darko1999 <dare162799@gmail.com>
2020-03-30 21:19:16 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const filmovi = require('./routes/filmovi')
+const serije = require('./routes/serije')
+const glumci = require('./routes/glumci')
+
+app.use('/api/v1/filmovi', filmovi)
+app.use('/api/v1/serije', serije)
+app.use('/api/v1/glumci', glumci)
+
+app.listen(4000, () => console.log('Server is listening on port 4000'))
